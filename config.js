24847bca8f204901ba6aaa790120c0d60a973022
JuliG13 <128094821+JulietaG13@users.noreply.github.com>
2024-05-27 11:02:43 -0300

    -

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.debug = process.env.DEBUG || true;
 
 config.mqtt  = {};
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || '#';
-config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '54.208.116.244';
+config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '3.91.252.8';
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
 
 config.mongodb = {};
